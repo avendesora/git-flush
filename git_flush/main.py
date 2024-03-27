a1d82fb22ed5a07efedba69ee049633dbf9cccb1
Nathan Patton <npatton@gmail.com>
2024-03-27 12:09:01 -0500

    WIP

--- git_flush/main.py
+++ git_flush/main.py
@@ -8,41 +8,59 @@ from git import InvalidGitRepositoryError, Repo
 app = typer.Typer()
 
 
-@app.command()
-def main():
+def _get_repo() -> Repo:
     current_directory = os.getcwd()
 
     try:
-        repo = Repo(current_directory)
-
-        # If there are any uncommitted changes, exit.
-        if repo.is_dirty():
-            raise BadParameter(f"{current_directory} has uncommitted changes")
-
-        # Get current branch name
-        branch = repo.active_branch.name
-
-        print(f"Current branch: {branch}")
-
-        if branch != "main":
-            print("Checking out main branch")
-            repo.git.checkout("main")
-
-        # Pull latest changes from remote
-        print("Pulling latest changes from remote")
-        repo.git.pull()
-
-        # Fetch all branches from remote
-        print("Fetching all branches from remote")
-        repo.git.fetch("--all")
-
-        if branch != "main":
-            # Delete the original branch
-            print(f"Deleting {branch}")
-            repo.git.branch("-D", branch)
-
+        return Repo(current_directory)
     except InvalidGitRepositoryError as e:
         raise BadParameter(f"{current_directory} is not a git repository") from e
+
+
+@app.command()
+def main(default_branch: str = "main", untracked_files: bool = True):
+    current_directory = os.getcwd()
+    repo = _get_repo()
+
+    # Check default branch
+    try:
+        repo.refs[default_branch]
+    except IndexError as e:
+        raise BadParameter(f"{default_branch} is not a valid branch name") from e
+
+    # Check for uncommitted changes
+    if repo.is_dirty(untracked_files=untracked_files):
+        raise BadParameter(f"{current_directory} has uncommitted changes")
+
+    # Get current branch name
+    branch = repo.active_branch.name
+
+    typer.echo(f"Current branch: {branch}")
+
+    # If there are changes that have not yet been pushed to the remote, exit.
+    if repo.head.commit not in repo.iter_commits(f"origin/{default_branch}"):
+        error_message = (
+            f"{current_directory} has changes that have not been pushed to the remote "
+            f"or merged into {default_branch}."
+        )
+        raise BadParameter(error_message)
+
+    if branch != default_branch:
+        typer.echo(f"Checking out {default_branch} branch")
+        repo.git.checkout(default_branch)
+
+    # Pull latest changes from remote
+    typer.echo("Pulling latest changes from remote")
+    repo.git.pull()
+
+    # Fetch all branches from remote
+    typer.echo("Fetching all branches from remote")
+    repo.git.fetch("--all")
+
+    if branch != "main":
+        # Delete the original branch
+        typer.echo(f"Deleting {branch}")
+        repo.git.branch("-D", branch)
 
 
 if __name__ == "__main__":
